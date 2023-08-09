f9f1884df7ab207e4bc5c0709f3987a37ef4d891
Stensis <irenenjuguna98@gmail.com>
2023-08-09 12:57:09 +0300

    added files structures layout

---
+++ src/Components/Constants/Colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+    PRIMARY: '#3498db',
+    SECONDARY: '#2ecc71',
+    WARNING: '#e67e22',
+    DANGER: '#e74c3c',
+    INFO: '#8e44ad',
+    // ... any other colors you want
+  };
+  
+  export default COLORS;
---
+++ src/Components/Constants/Routes.js
@@ -0,0 +1,9 @@
+// routes.js
+
+export const ROUTES = {
+    HOME: '/',
+    ABOUT: '/about',
+    PROFILE: '/profile/:id',
+    // ... other routes
+  };
+  
