1785150a0898ec1bea1ed5c132887278305181e1
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-10 00:12:36 +0300

    feat: 🚧 working on the button and sign in

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import TextRendering from "./Components/Shared/TextRendering";
 import SearchBar from "./Components/Shared/Search";
 
+
 export default function App() {
   return (
+
     <div style={{ backgroundColor: "whitesmoke", height: "100vh" }}>
+
       <TextRendering>Happy-Store-App</TextRendering>
-      <SearchBar/>
+      <SearchBar/> 
+
     </div>
   );
 }
