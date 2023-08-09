ff8b831c14a92ab809fc8925f1f6ca876e63498c
Stensis <irenenjuguna98@gmail.com>
2023-08-09 17:51:33 +0300

    added a search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import TextRendering from "./Components/Shared/TextRendering";
+import SearchBar from "./Components/Shared/Search";
 
 export default function App() {
   return (
     <div style={{ backgroundColor: "whitesmoke", height: "100vh" }}>
       <TextRendering>Happy-Store-App</TextRendering>
+      <SearchBar/>
     </div>
   );
-
 }
