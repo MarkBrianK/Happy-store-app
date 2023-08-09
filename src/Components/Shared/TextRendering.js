238ea06924b0b38efae10b8dbb0ef1244cbbe431
Stensis <irenenjuguna98@gmail.com>
2023-08-09 16:59:50 +0300

    added Text Rnderinf component to be used to render the apps text

--- src/Components/Shared/TextRendering.js
+++ src/Components/Shared/TextRendering.js
@@ -2,7 +2,7 @@ import COLORS from "../../Constants/Colors";
 
 export default function TextRendering({ children }) {
   return (
-    <div style={{ color: COLORS.blackColor, fontSize: 24, fontWeight: "bold" }}>
+    <div style={{ color: COLORS.blackColor, fontSize: 25, fontWeight: "bold" }}>
       {children}
     </div>
   );
