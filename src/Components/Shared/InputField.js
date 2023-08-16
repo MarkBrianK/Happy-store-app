efda7ac75c521a0d0ba241fd85adaff1e3d09845
Stensis <irenenjuguna98@gmail.com>
2023-08-16 13:12:26 +0300

    added children to inputfield

--- src/Components/Shared/InputField.js
+++ src/Components/Shared/InputField.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-export default function InputField({ label, type, placeholder, value, onChange }) {
-  return (
-    <div className="p-3 mb-1">
-      {children}
-
-    </div>
-  );
+export default function InputField({
+  label,
+  type,
+  placeholder,
+  value,
+  onChange,
+  children,
+}) {
+  return <div className="p-3 mb-1">{children}</div>;
 }
